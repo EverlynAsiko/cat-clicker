7c3e7d40e3a503c7783711913f8fd2c1b4235308
EverlynAsiko <asikoeverlyn@gmail.com>
2018-10-04 23:40:25 +0300

    Feature: Second cat added

--- js/app.js
+++ js/app.js
@@ -7,6 +7,13 @@ function getClicks() {
 	document.getElementById("clicks").innerHTML=clicks;
 }
 
-var catImage=document.getElementById("catImage");
+var cat1=document.getElementById("cat1");
 
-catImage.addEventListener("click", getClicks);
+cat1.addEventListener("click", getClicks);
+
+let parent=document.getElementById("main");
+let newElement=document.createElement("div");
+newElement.setAttribute("id", "cat2");
+newElement.innerHTML='<button id="cat1"><img src="images/cat2.jpg"></button><p>Number of clicks:<span id="clicks">0</span></p>'
+
+parent.appendChild(newElement);
