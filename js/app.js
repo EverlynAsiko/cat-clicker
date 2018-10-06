66c82907d6bd3a2bc53d6941fb664bd016aaf140
EverlynAsiko <asikoeverlyn@gmail.com>
2018-10-06 10:35:13 +0300

    Fix:Cleaner code for adding more cats

--- js/app.js
+++ js/app.js
@@ -1,27 +1,21 @@
 "use strict";
 
-let clicks=0;
-let clicks2=0;
+function catFunctionality(catName,clickId,imgSource) {
+	let clicks=0;
+	function getClicks() {
+		clicks++;
+		document.getElementById(clickId).innerHTML=clicks;
+	}
 
-function getClicks() {
-	clicks++;
-	document.getElementById("clicks").innerHTML=clicks;
+	let parent=document.getElementById("main");
+	let newElement=document.createElement("div");
+	newElement.innerHTML='<h3>'+catName+'</h3><button id="'+catName+'"><img src="'+imgSource+'"></button><p>Number of clicks:<span id="'+clickId+'">0</span></p>'
+
+	parent.appendChild(newElement);
+
+	let cat=document.getElementById(catName);
+	cat.addEventListener("click", getClicks);
 }
 
-let cat1=document.getElementById("cat1");
-
-cat1.addEventListener("click", getClicks);
-
-let parent=document.getElementById("main");
-let newElement=document.createElement("div");
-newElement.innerHTML='<h3>Cat 2</h3><button id="cat2"><img src="images/cat2.jpg"></button><p>Number of clicks:<span id="clicks2">0</span></p>'
-
-parent.appendChild(newElement);
-
-function getClicks2() {
-	clicks2++;
-	document.getElementById("clicks2").innerHTML=clicks2;
-}
-
-let cat2=document.getElementById("cat2");
-cat2.addEventListener("click", getClicks2);
+catFunctionality("Cat 1","Click1","images/cat.jpg");
+catFunctionality("Cat 2","Click2","images/cat2.jpg");
