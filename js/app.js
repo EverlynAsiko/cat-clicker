b27f616afac83dfcc2f70f5529cc32940e8d265f
EverlynAsiko <asikoeverlyn@gmail.com>
2018-10-06 09:18:42 +0300

    Feature: Second cat click counter

--- js/app.js
+++ js/app.js
@@ -1,19 +1,27 @@
 "use strict";
 
 let clicks=0;
+let clicks2=0;
 
 function getClicks() {
 	clicks++;
 	document.getElementById("clicks").innerHTML=clicks;
 }
 
-var cat1=document.getElementById("cat1");
+let cat1=document.getElementById("cat1");
 
 cat1.addEventListener("click", getClicks);
 
 let parent=document.getElementById("main");
 let newElement=document.createElement("div");
-newElement.setAttribute("id", "cat2");
-newElement.innerHTML='<button id="cat1"><img src="images/cat2.jpg"></button><p>Number of clicks:<span id="clicks">0</span></p>'
+newElement.innerHTML='<h3>Cat 2</h3><button id="cat2"><img src="images/cat2.jpg"></button><p>Number of clicks:<span id="clicks2">0</span></p>'
 
 parent.appendChild(newElement);
+
+function getClicks2() {
+	clicks2++;
+	document.getElementById("clicks2").innerHTML=clicks2;
+}
+
+let cat2=document.getElementById("cat2");
+cat2.addEventListener("click", getClicks2);
