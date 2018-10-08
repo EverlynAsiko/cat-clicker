7b08933c81d93271f85aae7435065bbc9fa5caa9
EverlynAsiko <asikoeverlyn@gmail.com>
2018-10-08 16:30:16 +0300

    Feature: Added cat list and cat images

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 "use strict";
 
+let catName=['Cat 1','Cat 2','Cat 3','Cat 4','Cat 5'];
+
+let parent=document.getElementById("catlist");
+let catlist='';
+for(let i = 0; i < catName.length; i++) {
+	catlist += '<div id="list_'+catName[i]+'">'+catName[i]+'</div>';
+}
+parent.innerHTML=catlist;
+
 function catFunctionality(catName,clickId,imgSource) {
 	let clicks=0;
 	function getClicks() {
@@ -7,7 +16,7 @@ function catFunctionality(catName,clickId,imgSource) {
 		document.getElementById(clickId).innerHTML=clicks;
 	}
 
-	let parent=document.getElementById("main");
+	let parent=document.getElementById("images");
 	let newElement=document.createElement("div");
 	newElement.innerHTML='<h3>'+catName+'</h3><button id="'+catName+'"><img src="'+imgSource+'"></button><p>Number of clicks:<span id="'+clickId+'">0</span></p>'
 
@@ -17,5 +26,5 @@ function catFunctionality(catName,clickId,imgSource) {
 	cat.addEventListener("click", getClicks);
 }
 
-catFunctionality("Cat 1","Click1","images/cat.jpg");
+//catFunctionality("Cat 1","Click1","images/cat.jpg");
 catFunctionality("Cat 2","Click2","images/cat2.jpg");
