da50cfd124f15350164c342749104a237bbd268b
EverlynAsiko <asikoeverlyn@gmail.com>
2018-10-08 16:54:47 +0300

    Feature:Cat image updates when cat list is clicked

--- js/app.js
+++ js/app.js
@@ -21,10 +21,29 @@ function catFunctionality(catName,clickId,imgSource) {
 	newElement.innerHTML='<h3>'+catName+'</h3><button id="'+catName+'"><img src="'+imgSource+'"></button><p>Number of clicks:<span id="'+clickId+'">0</span></p>'
 
 	parent.appendChild(newElement);
+	parent.replaceChild(newElement,parent.childNodes[0]);
 
 	let cat=document.getElementById(catName);
 	cat.addEventListener("click", getClicks);
 }
 
 //catFunctionality("Cat 1","Click1","images/cat.jpg");
-catFunctionality("Cat 2","Click2","images/cat2.jpg");
+//catFunctionality("Cat 2","Click2","images/cat2.jpg");
+//catFunctionality("Cat 3","Click3","images/cat3.jpg");
+//catFunctionality("Cat 4","Click4","images/cat4.jpg");
+//catFunctionality("Cat 5","Click5","images/cat5.jpg");
+document.getElementById("list_Cat 1").addEventListener("click",function() {
+	catFunctionality("Cat 1","Click1","images/cat.jpg");
+});
+document.getElementById("list_Cat 2").addEventListener("click",function() {
+	catFunctionality("Cat 2","Click2","images/cat2.jpg");
+});
+document.getElementById("list_Cat 3").addEventListener("click",function() {
+	catFunctionality("Cat 3","Click3","images/cat3.jpg");
+});
+document.getElementById("list_Cat 4").addEventListener("click",function() {
+	catFunctionality("Cat 4","Click4","images/cat4.jpg");
+});
+document.getElementById("list_Cat 5").addEventListener("click",function() {
+	catFunctionality("Cat 5","Click5","images/cat5.jpg");
+});
